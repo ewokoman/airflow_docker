eb9aa93c11b97624552d694346c27bb82e5b4685
ewokoman <ewokoman@yandex.ru>
2022-03-29 13:35:29 +0300

    First commit

---
+++ script/init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Создание БД
+sleep 10
+airflow upgradedb
+sleep 10
+airflow create_user -r Admin -u admin -f viktor -l ponomarev -p admin -e ewokoman@yandex.ru
+sleep 10
+# Запуск шедулера и вебсервера
+airflow scheduler & airflow webserver
